e7a0d3ed80380d9a63952d2ff4bbe3e7fd06dcb5
vikash sharma <vikashtiwari9893@gmail.com>
2025-03-04 02:08:24 +0530

    task managemnet app

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:5000/api';
+const baseURL = 'https://employee-management-1f4h.onrender.com/api';
 
 const api = axios.create({
   baseURL,
